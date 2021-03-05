38623f4c959444bfa1ac3b52dd11a7fd883f4184
vikash4824 <vamsivikash24@gmail.com>
2021-03-05 18:02:40 +0530

    Initial Commit

---
+++ 2012_AlertControler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2012_AlertControler
+//
+//  Created by mamidisetty Vikash on 05/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
